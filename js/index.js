9ba34fe3c8b230d144589a530ea1d7a214fd82c9
Daniel Ajadi <ajadidan@gmail.com>
2019-09-30 18:53:24 -0400

    Progress commit

--- js/index.js
+++ js/index.js
@@ -43,9 +43,11 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let imgCta = document.getElementById("cta-img");
 imgCta.setAttribute('src', siteContent["cta"]["img-src"])
+imgCta.alt = "Image of a code snippet"
 
 let imgMiddle = document.getElementById("middle-img");
 imgMiddle.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+imgMiddle.alt = "Image of code snippets across the screen"
 
 let ctaH1 = document.querySelector("h1");
 ctaH1.style.whiteSpace = 'pre';
@@ -65,7 +67,36 @@ let contactP1 = document.querySelector(".contact p");
 console.log(contactP1)
 contactP1.style.width = "200px";
 contactP1.style.whitespace = "pre";
-contactP1.textContent = "123 Way 456 Street Somewhere, USA\n \n 1-(888)-888-8888\n sales@greatidea.io"
+contactP1.textContent = "123 Way 456 Street Somewhere, USA\n 1-(888)-888-8888\n sales@greatidea.io"
+
+let navItem = document.getElementsByTagName('a');
+navItem[0].textContent = "Services"
+navItem[1].textContent = "Product"
+navItem[2].textContent = "Vision"
+navItem[3].textContent = "Features"
+navItem[4].textContent = "About"
+navItem[5].textContent = "Contact"
+
+const topContentText = document.querySelectorAll('.top-content .text-content');
+console.log(topContentText);
+topContentText[0].firstElementChild.textContent = "Features"
+topContentText[0].lastElementChild.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+topContentText[1].firstElementChild.textContent = "About"
+topContentText[1].lastElementChild.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const btmContentText = document.querySelectorAll('.bottom-content .text-content');
+console.log(btmContentText);
+btmContentText[0].firstElementChild.textContent = "Services"
+btmContentText[0].lastElementChild.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+btmContentText[1].firstElementChild.textContent = "Product"
+btmContentText[1].lastElementChild.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+btmContentText[2].firstElementChild.textContent = "Vision"
+btmContentText[2].lastElementChild.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
 
 
 
