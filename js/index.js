10168585fe83da62636b9be445d5212bce9d8c87
Daniel Ajadi <ajadidan@gmail.com>
2019-09-30 16:34:02 -0400

    Initial commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let imgCta = document.getElementById("cta-img");
+imgCta.setAttribute('src', siteContent["cta"]["img-src"])
